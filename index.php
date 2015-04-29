769389700fd2ba427f024577c683f042d8a07b90
f-delabre <hicham.hallou@gfpfrance.com>
2015-04-29 09:34:51 +0200

    Index -> Refactoring

--- index.php
+++ index.php
@@ -1,52 +1,47 @@
 <!DOCTYPE html>
 <html>
-<head>
+	<head>
     <script type="text/javascript" src="./script/jquery.js"></script>
     <script type="text/javascript" src="./script/callingcooker.js"></script>
     <link type="text/css" rel="stylesheet" href="./css/bootstrap.css">
     <link type="text/css" rel="stylesheet" href="./css/callingcooker.css">
     <title>CallingCooker</title>
     <meta charset="utf-8">
-</head>
-<body>
+	</head>
+	<body>
 
-
-<div class="logins">
-    <div class="col-md-2"></div>
-    <div class="col-md-4">
+		<div class="logins">
+    	<div class="col-md-4 col-md-offset-2">
         <div class="cooker">
-            <form action="requestLoginCooker.php" method="POST">
-                <p><input type="text" name="username" placeholder="identifiant"></p>
-                <p><input type="password" name="password" placeholder="mot de passe"></p>
-                <p><input type="submit" value="sign in"></p>
-            </form>
+        	<form action="requestLoginCooker.php" method="POST">
+          	<p><input type="text" name="username" placeholder="identifiant"></p>
+            <p><input type="password" name="password" placeholder="mot de passe"></p>
+            <p><input type="submit" value="sign in"></p>
+          </form>
         </div>
         <form action="RequestRegisterCooker.php" method="POST">
-            <p><input type="text" name="email" placeholder="email"></p>
-            <p><input type="text" name="username" placeholder="identifiant"></p>
-            <p><input type="password" name="password" placeholder="mot de passe"></p>
-            <p><input type="submit" value="Register"></p>
+        	<p><input type="text" name="email" placeholder="email"></p>
+          <p><input type="text" name="username" placeholder="identifiant"></p>
+          <p><input type="password" name="password" placeholder="mot de passe"></p>
+          <p><input type="submit" value="Register"></p>
         </form>
+			</div>
     </div>
-
 
     <div class="col-md-4">
-        <div class="client">
-            <form action="requestLoginClient.php" method="POST">
-                <p><input type="text" name="username" placeholder="identifiant"></p>
-                <p><input type="password" name="password" placeholder="mot de passe"></p>
-                <p><input type="submit" value="sign in"></p>
-            </form>
-        </div>
-        <form action="RequestRegisterClient.php" method="POST">
-            <p><input type="text" name="email" placeholder="email"></p>
-            <p><input type="text" name="username" placeholder="identifiant"></p>
-            <p><input type="password" name="password" placeholder="mot de passe"></p>
-            <p><input type="submit" value="Register"></p>
+    	<div class="client">
+      	<form action="requestLoginClient.php" method="POST">
+        	<p><input type="text" name="username" placeholder="identifiant"></p>
+          <p><input type="password" name="password" placeholder="mot de passe"></p>
+          <p><input type="submit" value="sign in"></p>
         </form>
+      </div>
+      <form action="RequestRegisterClient.php" method="POST">
+      	<p><input type="text" name="email" placeholder="email"></p>
+        <p><input type="text" name="username" placeholder="identifiant"></p>
+       	<p><input type="password" name="password" placeholder="mot de passe"></p>
+        <p><input type="submit" value="Register"></p>
+      </form>
     </div>
-    <div class="col-md-2"></div>
-</div>
-
-</body>
+	</body>
 </html>
